113b20dd519af7deb1a334feca3e8c39b3d4c917
Juliette Pretot <hi@juliette.sh>
2018-09-04 18:10:30 +0200

    add workaround for iOS full screen bug

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,11 +4,23 @@ import 'style/normalize.css'
 import 'style/custom-properties.css'
 import 'style/index.css'
 
-const Layout = props => (
-  <>
-    <Navbar activeName={props.activeName} />
-    <main className="main">{props.children}</main>
-  </>
-)
+class Layout extends React.Component {
+  componentDidMount() {
+    // iOS workaround: If run in fullscreen, force full height.
+    // see https://nicolas-hoizey.com/2015/02/viewport-height-is-taller-than-the-visible-part-of-the-document-in-some-mobile-browsers.html
+    if ('standalone' in window.navigator && window.navigator.standalone) {
+      document.querySelector('body').style.minHeight = '100vh'
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar activeName={this.props.activeName} />
+        <main className="main">{this.props.children}</main>
+      </>
+    )
+  }
+}
 
 export default Layout
