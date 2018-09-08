5a9ce3b0cf74f63fbb82320ed1f4d42f69f7632b
Juliette Pretot <hi@juliette.sh>
2018-09-08 11:01:35 +0200

    minify css on build with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
-const postcssPresetEnv = require(`postcss-preset-env`)
+const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
 
 module.exports = () => ({
   plugins: [
     postcssPresetEnv({
       stage: 0
+    }),
+    cssnano({
+      preset: ['default', { discardComments: { removeAll: true } }]
     })
   ]
 })
