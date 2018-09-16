7363119f681f5f0d8bb1f772c4921d8610ea7b23
Juliette Pretot <hi@juliette.sh>
2018-09-16 16:22:37 +0200

    remove add to home screen optimizations for maintainibility

--- src/html.js
+++ src/html.js
@@ -57,41 +57,6 @@ class HTML extends React.Component {
             href="/favicons/favicon-32x32.png"
           />
           <link rel="shortcut icon" href="/favicons/favicon.ico" />
-          <link
-            href="/splashscreens/iphone5_splash.png"
-            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/iphone6_splash.png"
-            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/iphoneplus_splash.png"
-            media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/iphonex_splash.png"
-            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/ipad_splash.png"
-            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/ipadpro1_splash.png"
-            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="/splashscreens/ipadpro2_splash.png"
-            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
           <style
             dangerouslySetInnerHTML={{
               __html: webFonts
