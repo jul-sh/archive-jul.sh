54b012ab1e66aa54bbdc45d9faa3e754fc80c610
Juliette Pretot <hi@juliette.sh>
2018-12-12 12:23:48 +0100

    Improve link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -6,10 +6,23 @@ const isUrl = (url: string) => urlMatcher.test(url)
 
 interface LinkProps {
   to: string
+  className?: string
 }
 
-const CustomLink: React.FunctionComponent<LinkProps> = ({ to }) =>
+const CustomLink: React.FunctionComponent<LinkProps> = ({
+  to,
+  className,
+  children
+}) =>
   // eslint-disable-next-line jsx-a11y/anchor-has-content
-  isUrl(to) ? <a href={to} /> : <Link to={to} />
+  isUrl(to) ? (
+    <a href={to} className={className}>
+      {children}
+    </a>
+  ) : (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  )
 
 export default CustomLink
