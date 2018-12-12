5a08d1393b2312ed74165961a6ce7cbacb3abb6b
Juliette Pretot <hi@juliette.sh>
2018-12-12 20:06:11 +0100

    rewrite the html template in typescript

---
+++ src/html.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+
+const FONTS = {
+  regular: '/fonts/jregular.woff2',
+  bold: '/fonts/jbold.woff2',
+  sans: '/fonts/jsans.woff2'
+}
+
+const webFonts = `
+@font-face {
+  font-family: 'Juliette';
+  src: url('${FONTS.regular}') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Juliette';
+  src: url('${FONTS.bold}') format('woff2');
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Juliette Sans';
+  src: url('${FONTS.sans}') format('woff2');
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
+`
+
+interface HtmlProps {
+  htmlAttributes: any
+  headComponents: any
+  preBodyComponents: any
+  body: any
+  bodyAttributes: any
+  postBodyComponents: any
+}
+
+const HTML: React.FunctionComponent<HtmlProps> = props => (
+  <html lang="en" {...props.htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <title>Juliette Pretot</title>
+      <meta name="description" content="Frontend Engineer" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <link
+        rel="preload"
+        href={FONTS.regular}
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href={FONTS.bold}
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href={FONTS.sans}
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <meta name="theme-color" content="#101722" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <style
+        dangerouslySetInnerHTML={{
+          __html: webFonts
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html:
+            '/*\n \n \n🔵🔵🔵🔵🔵🔴🔴🔴🔴🔴🔴\n🔵🔵🔵🔵🔵🔴🔴🔴🔴🔴🔴\n🔵🔵🔵🔵🔵🔴🔴🔴🔴🔴🔴\n🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴\n🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴\n🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴 \n \n \nHey astronaut. You can view the uncompiled soure code at https://github.com/juliettepretot/juliette.sh :) \n \n \n*/'
+        }}
+      />
+      {props.headComponents}
+    </head>
+    <body {...props.bodyAttributes}>
+      {props.preBodyComponents}
+      <div
+        key={'body'}
+        id="___gatsby"
+        dangerouslySetInnerHTML={{ __html: props.body }}
+      />
+      {props.postBodyComponents}
+    </body>
+  </html>
+)
+
+export default HTML
